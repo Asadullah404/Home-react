44c3cc5bf0c420e0778fa792acaafe43d8982cf0
Asadullah404 <151784066+Asadullah404@users.noreply.github.com>
2025-01-28 00:56:46 +0500

    Update Genpdf.jsx

--- src/pages/Genpdf.jsx
+++ src/pages/Genpdf.jsx
@@ -38,7 +38,7 @@ const GenPDF = () => {
   const doc = new jsPDF();
   const pageWidth = doc.internal.pageSize.width;
   const pageHeight = doc.internal.pageSize.height;
-  const logo = 'public/logo.PNG'; // Path to your logo
+  const logo = '/vite.PNG'; // Path to your logo
 
   let y = 50; // Starting y-coordinate after logo
 
